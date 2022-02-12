512c4be0ae17ff51e4d59c03e4f0d5c3730d4b0f
aarafat27 <arafatedu11@gmail.com>
2022-02-12 20:49:27 +0600

    first commit

---
+++ DigitalClock/main.py
@@ -0,0 +1,21 @@
+# A digital clock
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title('Digital Clock')
+
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+
+label = Label(root, font=('TkDefaultFont', 80), background='black', foreground='cyan')
+label.pack(anchor='center')
+time()
+
+mainloop()
