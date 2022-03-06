883deddb9b7cdff9356f705fd461a0c12e71c811
Aarafat Islam <99079792+aarafat27@users.noreply.github.com>
2022-03-06 19:49:54 +0600

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+
+dictionary = {'Name': 'Aarafat', 'Age': 19, 'Class': 'First'}
+dictionary['Age'] = 20; 
+dictionary['College'] = "Cambrian";
+
+
+print ("dictionary: ", dictionary)
+print ("dictionary['Age']: ", dictionary['Age'])
+print ("dictionary['School']: ", dictionary['College'])
+
+del dictionary['Name'];
+
+print ("dictionary: ", dictionary)
