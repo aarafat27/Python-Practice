2fbf2d2030e9a574e139c1dc380b9fc015d6a03e
Aarafat Islam <99079792+aarafat27@users.noreply.github.com>
2022-03-06 19:58:19 +0600

    Add files via upload

---
+++ List.py
@@ -0,0 +1,13 @@
+list_1 = ['CSE', 'EEE', 2020, 2021, 'USA', 'Russia']
+
+print('Type : ',type(list_1))
+print ('list_1 :',list_1)
+print('list_1 len :',len(list_1))
+
+del list_1[2]
+
+print ("After deleting value at index 2 : ")
+print ('list_1 :',list_1)
+
+print(list_1[2])
+print('list_1 len :',len(list_1))
